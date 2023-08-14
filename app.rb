dd58cbe53c70e890cdbc1e8b66b9f2492bdeccea
Jorge Pioquinto <129575128+Jorge-Pioquinto@users.noreply.github.com>
2023-08-14 07:22:40 +0000

    Done

--- app.rb
+++ app.rb
@@ -1,10 +1,28 @@
 require "sinatra"
 require "sinatra/reloader"
+require 'json'
+require 'http'
 
 get("/") do
+  currencies_data = JSON.parse(HTTP.get('https://api.exchangerate.host/symbols'))
+  @currencies = currencies_data['symbols']
+
   erb(:homepage)
 end
 
-get("/USD") do
-  erb(:convert_usd)
-end 
+get '/:from_currency' do
+  @from_currency = params[:from_currency]
+  currencies_data = JSON.parse(HTTP.get('https://api.exchangerate.host/symbols'))
+  @currencies = currencies_data['symbols']
+
+  erb(:convert)
+end
+
+get '/:from_currency/:to_currency' do
+  @from_currency = params[:from_currency]
+  @to_currency = params[:to_currency]
+  conversion_data = JSON.parse(HTTP.get("https://api.exchangerate.host/convert?from=#{@from_currency}&to=#{@to_currency}"))
+  @rate = conversion_data['result']
+
+  erb(:conversion)
+end
